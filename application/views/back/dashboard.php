e8c2a5e8950bd7e110f109b2a713b4578e40a31c
adji142 <prasetyoajiw@gmail.com>
2018-03-19 17:01:35 +0700

    edit little bit
    Signed-off-by: adji142 <prasetyoajiw@gmail.com>

--- application/views/back/dashboard.php
+++ application/views/back/dashboard.php
@@ -38,7 +38,24 @@
         </div>
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
+          <?php
+            $active = $this->db->query("select * from app_post where id_reg = $id_reg");
+          ?>
           <div class="small-box bg-green">
+            <div class="inner">
+              <h3><?php echo $active->num_rows();?></h3>
+
+              <p>All Promo</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-ios-infinite"></i>
+            </div>
+            <a href="<?php echo base_url('back/dashboard/ongoing')?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- <div class="col-lg-3 col-xs-6"> -->
+          <!-- small box -->
+          <!-- <div class="small-box bg-green">
             <div class="inner">
               <h3>53<sup style="font-size: 20px">%</sup></h3>
 
@@ -48,11 +65,11 @@
               <i class="ion ion-stats-bars"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
+          </div> -->
+        <!-- </div> -->
+        <!-- <div class="col-lg-3 col-xs-6"> -->
           <!-- small box -->
-          <div class="small-box bg-yellow">
+          <!-- <div class="small-box bg-yellow">
             <div class="inner">
               <h3>44</h3>
 
@@ -62,11 +79,11 @@
               <i class="ion ion-person-add"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
+          </div> -->
+        <!-- </div> -->
+        <!-- <div class="col-lg-3 col-xs-6"> -->
           <!-- small box -->
-          <div class="small-box bg-red">
+          <!-- <div class="small-box bg-red">
             <div class="inner">
               <h3>65</h3>
 
@@ -76,21 +93,21 @@
               <i class="ion ion-pie-graph"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
+          </div> -->
+        <!-- </div> -->
     </div>
     
-    <div class="row">
+    <!-- <div class="row">
       <div class="col-lg-12 connectedSortable">
         <div class="nav-tabs-custom">
-            <div class="tab-content no-padding">
+            <div class="tab-content no-padding"> -->
               <!-- Morris chart - Sales -->
-              <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
+              <!-- <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
               <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
             </div>
           </div>
       </div>
-    </div>
+    </div> -->
   </section>
 </div>
 <?php
